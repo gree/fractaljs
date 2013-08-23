1d8dfa7820731c3260ac4cd27182b0edcbb3268e
Xiaolu Liu <xiaolu.liu@gree.net>
2013-08-23 18:07:37 +0900

    simple httpserver for demo

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+var port = 8765;
+
+app.configure(function () {
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(app.router);
+  // app.use(express.static(__dirname + '/../gui'));
+  app.use(express.static(__dirname));
+  app.use(express.directory(__dirname));
+});
+
+app.listen(port);
