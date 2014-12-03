e21a3e477d0928e3574c887e34fee8fbda8e15f1
Xiaolu Liu <xiaolu.liu@g-pc-3651.intra.gree-office.net>
2014-12-03 14:06:38 +0900

    add data grid exmaple

--- examples/main.js
+++ examples/main.js
@@ -6,3 +6,25 @@ F('main', F.app.Router.extend({
   },
 }));
 
+var examples = [
+  "markdown",
+  "gitrepo",
+  { "todomvc": "todomvc.html" },
+  "datagrid",
+];
+
+F("list", F.Component.extend({
+  getData: function(cb) {
+    var data = examples.map(function(v){
+      if (typeof(v) === "string") {
+        return { href: "#" + v, name: v }
+      } else {
+        for (var i in v) {
+          return { href: v[i], name: i };
+        }
+      }
+    });
+    cb({ examples: data });
+  }
+}));
+
